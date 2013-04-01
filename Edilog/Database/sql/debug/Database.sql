7688e2199998b8562a90900bbf885041abf8e499
rfontaine <rfontaine@epsi.fr>
2013-04-01 17:59:32 +0200

    Début de Maquettage pour le portail client - Ajout du template, maquette de la page d'authentification

---
+++ Edilog/Database/sql/debug/Database.sql
@@ -0,0 +1,214 @@
+﻿/*
+Script de déploiement pour Database
+*/
+
+GO
+SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON;
+
+SET NUMERIC_ROUNDABORT OFF;
+
+
+GO
+:setvar DatabaseName "Database"
+:setvar DefaultDataPath ""
+:setvar DefaultLogPath ""
+
+GO
+:on error exit
+GO
+USE [master]
+GO
+IF (DB_ID(N'$(DatabaseName)') IS NOT NULL
+    AND DATABASEPROPERTYEX(N'$(DatabaseName)','Status') <> N'ONLINE')
+BEGIN
+    RAISERROR(N'L''état de la base de données cible, %s, n''a pas la valeur ONLINE. Pour effectuer un déploiement sur cette base de données, son état doit être ONLINE.', 16, 127,N'$(DatabaseName)') WITH NOWAIT
+    RETURN
+END
+
+GO
+IF (DB_ID(N'$(DatabaseName)') IS NOT NULL) 
+BEGIN
+    ALTER DATABASE [$(DatabaseName)]
+    SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE [$(DatabaseName)];
+END
+
+GO
+PRINT N'Création de $(DatabaseName)...'
+GO
+CREATE DATABASE [$(DatabaseName)]
+    ON 
+    PRIMARY(NAME = [EdilogBDD], FILENAME = '$(DefaultDataPath)$(DatabaseName).mdf', FILEGROWTH = 1024 KB)
+    LOG ON (NAME = [EdilogBDD_log], FILENAME = '$(DefaultLogPath)$(DatabaseName)_log.ldf', MAXSIZE = 2097152 MB, FILEGROWTH = 10 %) COLLATE French_CI_AS
+GO
+EXECUTE sp_dbcmptlevel [$(DatabaseName)], 100;
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET ANSI_NULLS ON,
+                ANSI_PADDING ON,
+                ANSI_WARNINGS ON,
+                ARITHABORT ON,
+                CONCAT_NULL_YIELDS_NULL ON,
+                NUMERIC_ROUNDABORT OFF,
+                QUOTED_IDENTIFIER ON,
+                ANSI_NULL_DEFAULT ON,
+                CURSOR_DEFAULT LOCAL,
+                RECOVERY FULL,
+                CURSOR_CLOSE_ON_COMMIT OFF,
+                AUTO_CREATE_STATISTICS ON,
+                AUTO_SHRINK OFF,
+                AUTO_UPDATE_STATISTICS ON,
+                RECURSIVE_TRIGGERS OFF 
+            WITH ROLLBACK IMMEDIATE;
+        ALTER DATABASE [$(DatabaseName)]
+            SET AUTO_CLOSE OFF 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET ALLOW_SNAPSHOT_ISOLATION OFF;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET READ_COMMITTED_SNAPSHOT OFF;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET AUTO_UPDATE_STATISTICS_ASYNC OFF,
+                PAGE_VERIFY NONE,
+                DATE_CORRELATION_OPTIMIZATION OFF,
+                DISABLE_BROKER,
+                PARAMETERIZATION SIMPLE,
+                SUPPLEMENTAL_LOGGING OFF 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+IF IS_SRVROLEMEMBER(N'sysadmin') = 1
+    BEGIN
+        IF EXISTS (SELECT 1
+                   FROM   [master].[dbo].[sysdatabases]
+                   WHERE  [name] = N'$(DatabaseName)')
+            BEGIN
+                EXECUTE sp_executesql N'ALTER DATABASE [$(DatabaseName)]
+    SET TRUSTWORTHY OFF,
+        DB_CHAINING OFF 
+    WITH ROLLBACK IMMEDIATE';
+            END
+    END
+ELSE
+    BEGIN
+        PRINT N'Impossible de modifier les paramètres de base de données. Vous devez être administrateur système pour appliquer ces paramètres.';
+    END
+
+
+GO
+IF IS_SRVROLEMEMBER(N'sysadmin') = 1
+    BEGIN
+        IF EXISTS (SELECT 1
+                   FROM   [master].[dbo].[sysdatabases]
+                   WHERE  [name] = N'$(DatabaseName)')
+            BEGIN
+                EXECUTE sp_executesql N'ALTER DATABASE [$(DatabaseName)]
+    SET HONOR_BROKER_PRIORITY OFF 
+    WITH ROLLBACK IMMEDIATE';
+            END
+    END
+ELSE
+    BEGIN
+        PRINT N'Impossible de modifier les paramètres de base de données. Vous devez être administrateur système pour appliquer ces paramètres.';
+    END
+
+
+GO
+USE [$(DatabaseName)]
+GO
+IF fulltextserviceproperty(N'IsFulltextInstalled') = 1
+    EXECUTE sp_fulltext_database 'enable';
+
+
+GO
+/*
+ Modèle de script de prédéploiement							
+--------------------------------------------------------------------------------------
+ Ce fichier contient des instructions SQL qui seront exécutées avant le script de compilation.	
+ Utilisez la syntaxe SQLCMD pour inclure un fichier dans le script de prédéploiement.			
+ Exemple :      :r .\monfichier.sql								
+ Utilisez la syntaxe SQLCMD pour référencer une variable dans le script de prédéploiement.		
+ Exemple :      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+GO
+-- Étape de refactorisation pour mettre à jour le serveur cible avec des journaux de transactions déployés
+CREATE TABLE  [dbo].[__RefactorLog] (OperationKey UNIQUEIDENTIFIER NOT NULL PRIMARY KEY)
+GO
+sp_addextendedproperty N'microsoft_database_tools_support', N'refactoring log', N'schema', N'dbo', N'table', N'__RefactorLog'
+GO
+
+GO
+/*
+Modèle de script de post-déploiement							
+--------------------------------------------------------------------------------------
+ Ce fichier contient des instructions SQL qui seront ajoutées au script de compilation.		
+ Utilisez la syntaxe SQLCMD pour inclure un fichier dans le script de post-déploiement.			
+ Exemple :      :r .\monfichier.sql								
+ Utilisez la syntaxe SQLCMD pour référencer une variable dans le script de post-déploiement.		
+ Exemple :      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        DECLARE @VarDecimalSupported AS BIT;
+        SELECT @VarDecimalSupported = 0;
+        IF ((ServerProperty(N'EngineEdition') = 3)
+            AND (((@@microsoftversion / power(2, 24) = 9)
+                  AND (@@microsoftversion & 0xffff >= 3024))
+                 OR ((@@microsoftversion / power(2, 24) = 10)
+                     AND (@@microsoftversion & 0xffff >= 1600))))
+            SELECT @VarDecimalSupported = 1;
+        IF (@VarDecimalSupported > 0)
+            BEGIN
+                EXECUTE sp_db_vardecimal_storage_format N'$(DatabaseName)', 'ON';
+            END
+    END
+
+
+GO
+ALTER DATABASE [$(DatabaseName)]
+    SET MULTI_USER 
+    WITH ROLLBACK IMMEDIATE;
+
+
+GO
--- Edilog/ViewASP/Default.aspx
+++ Edilog/ViewASP/Default.aspx
@@ -4,14 +4,69 @@
 <asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
 </asp:Content>
 <asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
-    <h2>
-        Bienvenue dans ASP.NET!
-    </h2>
-    <p>
-        Pour en savoir plus sur ASP.NET, consultez <a href="http://www.asp.net" title="Site Web ASP.NET">www.asp.net</a>.
-    </p>
-    <p>
-        Vous pouvez également trouver de la <a href="http://go.microsoft.com/fwlink/?LinkID=152368"
-            title="Documentation ASP.NET sur MSDN">documentation sur ASP.NET sur MSDN</a>.
-    </p>
+    <!-- Page wrap -->
+	        	<div id="page-wrap">
+	        	
+	        		<!-- Front slider -->
+	        		<div id="front-slides">
+						<div class="slides_container">
+							<div class="slide">
+								<a href="http://www.luiszuno.com" target="blank"><img src="img/dummies/01.jpg"  alt="pic" width="940" height="360"  /></a>
+								<div class="caption">Visit my site at luiszuno.com and get freebies each week </div>
+							</div>
+							<div class="slide">
+								<a href="http://themeforest.net/user/Ansimuz/portfolio?ref=ansimuz" target="blank"><img src="img/dummies/02.jpg" alt="pic" width="940" height="360"  /></a>
+								<div class="caption">Support the freebies buying premium themes at themeforest.com</div>
+							</div>
+							<div class="slide">
+								<img src="img/dummies/940x370.jpg" alt="pic" width="940" height="360"  />
+								<div class="caption">Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est tristique senectus et netus et malesuada.</div>
+							</div>
+						</div>
+						
+						<div id="front-slides-cover"></div>
+							
+						<!-- Headline -->
+						<div id="headline"><h6></h6></div>
+						<!-- ENDS Headline -->	
+					
+					</div>
+					<!-- ENDS Front slider -->
+					
+					
+					<!-- Reel slider -->
+	        		<div id="reel">
+						<div class="slides_container">
+							<div class="slide-box">
+								<div class="box-container">
+									<img src="img/mono-icons/folder32.png" class="box-icon" alt="pic"/>
+									<h6>Dernières Infos</h6>
+									<div class="box-divider"></div>Tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est tristique senectus et netus et malesuada.
+								</div>
+								<div class="box-container">
+									<img src="img/mono-icons/heart32.png" class="box-icon" alt="pic"/>
+									<h6>Edilog</h6>
+									<div class="box-divider"></div>Tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est tristique senectus et netus et malesuada.
+								</div>
+								<div class="box-container">
+									<img src="img/mono-icons/lightbulb32.png" class="box-icon" alt="pic"/>
+									<h6>Espace Client</h6>
+									<div class="box-divider"></div>
+                                    <label>Identifiant :</label>
+                                    <ul>
+                                        <li>
+                                            
+                                        </li>
+                                    </ul>
+                                    <input type="email" id="email_auth" name="email_auth" required />
+                                    <label>Mot de passe :</label>
+                                    <input type="password" id="password_auth" name="password_auth" required />
+								</div>
+							</div>
+						</div>
+						
+					</div>
+					<!-- ENDS Reel slider -->
+	        	</div>
+	        	<!-- ENDS Page wrap -->
 </asp:Content>
